f68be8f921bea1af374da5526e40d6025d0e050c
CodaSirrah <harristurner97@gmail.com>
2021-03-28 16:32:44 +0100

    Improved read status functionality and a few other things.

--- books.js
+++ books.js
@@ -11,6 +11,7 @@ let pagesField = document.getElementById("pagesField");
 let pagesFieldLabel = document.getElementById("pagesFieldLabel");
 let submitBtn = document.querySelector("#submit");
 let readCheckBox = document.querySelector("#read");
+let htmlArea = document.querySelector("html");
 
 
 // Object Constructor
@@ -21,10 +22,12 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
-addBookToLibrary("Empire of Silver", "Conn Iggulden", 573, true);
-addBookToLibrary("Colorless Tsukuru Tazaki and his Years of Pilgrimage", "Haruki Murakami", 613, true);
-addBookToLibrary("Heroes", "Stephen Fry", 411, false);
+// Template books for display before adding local cache
+addBookToLibrary("Empire of Silver", "Conn Iggulden", 573, "true");
+addBookToLibrary("Colorless Tsukuru Tazaki and his Years of Pilgrimage", "Haruki Murakami", 613, "true");
+addBookToLibrary("Heroes", "Stephen Fry", 411, "false");
 
+// loops over MyLibrary objects to display each book. Creates a card and information based on each objects key values.
 function displayCurrentBooks() {
     for (i = 0; i < myLibrary.length; i++) {
         let createCard = document.createElement("section");
@@ -32,37 +35,30 @@ function displayCurrentBooks() {
         let createAuthor = document.createElement("p");
         let createPages = document.createElement("p");
         let createRead = document.createElement("button");
-    
         // write information.
         createTitle.innerHTML = myLibrary[i].title;
         createAuthor.innerHTML = "Author: " + myLibrary[i].author;
         createPages.innerHTML = "Pages: " + myLibrary[i].pages;
         createRead.innerHTML = "Read?";
-    
+
+        createRead.setAttribute("data-index", i);
+        
         // styling.
         createCard.classList.add("bookCard");
         createTitle.classList.add("itemSpacing")
         createAuthor.classList.add("itemSpacing")
         createPages.classList.add("itemSpacing")
         createRead.classList.add("itemSpacing");
-        if (myLibrary[i].read === true) 
-            createCard.classList.add("bookRead");
-        
-
         createTitle.classList.add("cardTitle");
         createAuthor.classList.add("cardAuthor");
         createPages.classList.add("cardPages");
         createRead.classList.add("cardRead");
-
-       
-
-        
+        if (myLibrary[i].read === "true" || myLibrary[i].read === "on")  createCard.classList.add("bookRead");
         // appending.
         createCard.appendChild(createTitle);
         createCard.appendChild(createAuthor);
         createCard.appendChild(createPages);
         createCard.appendChild(createRead);
-    
         // adding to document.
         gridContainer.appendChild(createCard);
     }
@@ -75,8 +71,8 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read)); 
 };
 
-function displayBooks() {
-
+// create a card and information based on forms input values. Add to myLibrary array.
+function displayNewBook() {
     let lastBook = myLibrary.pop();
     myLibrary.push(lastBook);
     // create items.
@@ -85,15 +81,12 @@ function displayBooks() {
     let createAuthor = document.createElement("p");
     let createPages = document.createElement("p");
     let createRead = document.createElement("button");
-
     // write information.
     createTitle.innerHTML = lastBook.title;
     createAuthor.innerHTML = "Author: " + lastBook.author;
     createPages.innerHTML = "Pages: " + lastBook.pages;
     createRead.innerHTML = "Read?";
 
-
-    
     // styling.
     createCard.classList.add("bookCard");
     createTitle.classList.add("itemSpacing")
@@ -101,15 +94,28 @@ function displayBooks() {
     createPages.classList.add("itemSpacing")
     createRead.classList.add("itemSpacing");
     createRead.classList.add("cardRead");
-    
+    if (lastBook.read === "true" || lastBook.read === "on") createCard.classList.add("bookRead");
+
     // appending.
-    createCard.appendChild(createTitle);
+    createCard.appendChild(createTitle);    
     createCard.appendChild(createAuthor);
     createCard.appendChild(createPages);
     createCard.appendChild(createRead);
 
     // adding to document.
     gridContainer.appendChild(createCard);
+
+    createRead.addEventListener("mouseenter", () => {
+        createRead.style.cssText = "background-color: rgba(101, 191, 230, 1); cursor: pointer; border: 2px solid black;"
+    });
+
+    createRead.addEventListener("mouseleave", () => {
+        createRead.style.cssText = "";
+    });
+
+    createRead.addEventListener("click", () => {
+        createCard.classList.toggle("bookRead");
+    });
     
 };
 function toggleForm() {
@@ -119,6 +125,7 @@ function toggleForm() {
 
 addBookBtn.addEventListener("click", () => {
     toggleForm();
+    resetForm();
 })
 
 titleField.addEventListener("input", () => {
@@ -139,53 +146,73 @@ pagesField.addEventListener("input", () => {
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (myLibrary.title) {
-        for (i = 0; i < myLibrary.length; i++) {
-            if (myLibrary[i].title === titleField.value) return alert("That book has already been added.");
-        }
+    for (i = 0; i < myLibrary.length; i++) {
+        if (titleField.value === myLibrary[i].title) return alert("That book has already been added."); 
     }
     newBook();
     resetForm();
     toggleForm();
     console.log(myLibrary);
-    displayBooks();
+    displayNewBook();
 })
 
-
 function newBook() {
-let titleData = titleField.value;
-let authorData = authorField.value;
-let pagesData = parseInt(pagesField.value);
-let readData = readCheckBox.value;
-return addBookToLibrary(titleData, authorData, pagesData, readData);
+    let titleData = titleField.value;
+    let authorData = authorField.value;
+    let pagesData = parseInt(pagesField.value);
+    let readData = readCheckBox.value;
+    return addBookToLibrary(titleData, authorData, pagesData, readData);
 }; 
 
+// resets form and hides.
 function resetForm() {
     titleField.value = "";
     authorField.value = "";
     pagesField.value = "";
+    readCheckBox.checked = false;
     readCheckBox.value = false;
     titleFieldLabel.classList.remove("hideLabel");
     authorFieldLabel.classList.remove("hideLabel");
     pagesFieldLabel.classList.remove("hideLabel");
-}
+};
 
 document.querySelectorAll("button").forEach(item => {
     item.addEventListener("mouseenter", () => {
         item.style.cssText = "background-color: rgba(101, 191, 230, 1); cursor: pointer; border: 2px solid black;"
     })
-
 });
 
 document.querySelectorAll("button").forEach(item => {
     item.addEventListener("mouseleave", () => {
         item.style.cssText = "";
     })
-
 });
-
 document.querySelectorAll(".cardRead").forEach(item => {
     item.addEventListener("click", () => {
         item.parentElement.classList.toggle("bookRead");
     })
 });
+
+// close form if click outside of form element or descendant.
+document.addEventListener("click", function(e)  {
+    let clickInside = form.contains(e.target);
+    let clickButton = addBookBtn.contains(e.target);
+        if (clickInside === false && clickButton === false && !(form.classList.contains("hidden"))) {
+            resetForm();
+            toggleForm();
+        
+    }
+});
+
+// assigns proper value to checkbox when clicked.
+readCheckBox.addEventListener("click", () => {
+    if (readCheckBox.value === "false" || readCheckBox.vallue === false || readCheckBox.value === "off") {
+        return readCheckBox.value = true;
+    } else {
+        return readCheckBox.value = false;
+    }
+});
+
+Book.prototype.readStatus = function(e) {
+    
+}
